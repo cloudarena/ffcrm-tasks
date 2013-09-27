949c800a5c836956d84aa647244a04a1ca8d805e
Road Tang <roadtang@gmail.com>
2013-09-27 18:06:33 +0800

    fix the add comment error for task with specific time

--- lib/ffcrm-tasks/hooks.rb
+++ lib/ffcrm-tasks/hooks.rb
@@ -7,5 +7,12 @@ ActiveSupport.on_load(:fat_free_crm_task) do
     def email_ids
       []
     end
+
+    # fix the task mode's bug. only new record
+    def specific_time
+      parse_calendar_date if self.bucket == "specific_time" and self.calendar
+    rescue ArgumentError
+      errors.add(:calendar, :invalid_date)
+    end
   end
 end
