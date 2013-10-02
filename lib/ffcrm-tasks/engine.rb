b29af19c4b85e40f6889152c20a77013e1a5cd0b
Road Tang <roadtang@gmail.com>
2013-10-03 04:50:12 +0800

    refine the engine loader order so that production mode and test mode works well

--- lib/ffcrm-tasks/engine.rb
+++ lib/ffcrm-tasks/engine.rb
@@ -7,13 +7,20 @@ module FfcrmTasks
     def eager_load!
       # empty so that the engine's stuff is not loaded in primary rails eager_load process
     end
-    
+
+    # 
+    #   Load all stuff that belongs to ffcrm-tasks engine. 
+    #
     def engine_eager_load! 
       railties.all(&:eager_load!)
       config.eager_load_paths.each do |load_path|
         matcher = /\A#{Regexp.escape(load_path)}\/(.*)\.rb\Z/
         Dir.glob("#{load_path}/**/*.rb").sort.each do |file|
-          require_dependency file.sub(matcher, '\1')
+          # remove the previous class (if exists) and require again (with new load path/order)
+          name = file.sub(matcher, '\1')
+          class_name = ActiveSupport::Inflector.camelize name
+          Object.send(:remove_const, class_name) if Object.const_defined?(class_name)
+          require_dependency name
         end
       end
     end
@@ -23,7 +30,7 @@ module FfcrmTasks
     # Here is trick to override the rails applicatioin's controller. 
     # notice it's not good idea to override the main application, since it's fighting back to framework rules. 
     # so use it with caution! and use only in case that you really understand the Rails bootstrap process clearly.
-    # - roadt.
+    # - Frank.
     #
     initializer :override_app_autoloads_paths do
       mod_all_autoload_paths  = _all_autoload_paths
@@ -35,6 +42,7 @@ module FfcrmTasks
         ActiveSupport::Dependencies.autoload_paths.unshift(*mod_all_autoload_paths)
         ActiveSupport::Dependencies.autoload_once_paths -= mod_all_autoload_once_paths
         ActiveSupport::Dependencies.autoload_once_paths.unshift(*mod_all_autoload_once_paths)
+#        ActiveSupport::Dependencies.clear
         mod_engine.engine_eager_load!
       end
     end
